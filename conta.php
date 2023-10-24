1aa331d33132b6bfd66424b1a773bc183109d2ad
GustavoVFA <gustavovfamorim@gmail.com>
2023-10-23 21:11:25 -0300

    index, getter, setter

---
+++ conta.php
@@ -0,0 +1,27 @@
+<?php 
+
+class Conta{
+    
+    private $valor;
+    private $gorjeta;
+
+
+    function setValor($valor){
+        $this->valor = $valor;
+    }
+
+    function getValor(){
+        return $this->valor;
+    }
+
+    function setGorjeta($gorjeta){
+        $this->gorjeta = $gorjeta;
+    }
+
+    function getGorjeta(){
+        return $this->gorjeta;
+    }
+   
+}
+
+
